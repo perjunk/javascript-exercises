68aac60019708b36502dba479d08960e52c75458
Paul Berger <paulberger56@gmail.com>
2023-06-04 17:44:24 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let c = (5/9)*(temp - 32);
+  c = Math.round(c *10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let f = (temp *(9/5)) + 32;
+  f = Math.round(f*10) /10;
+  return f;
 };
 
 // Do not edit below this line
