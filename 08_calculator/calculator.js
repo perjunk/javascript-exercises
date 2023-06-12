4b310e610ad641dfd911f0aea4541139e38f1252
Paul Berger <paulberger56@gmail.com>
2023-06-11 20:05:47 -0400

    Fill out calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  array.forEach(number =>  {
+    sum *= number;
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let start = 1;
+  for(i = 1; i <= number; i++) {
+    start *= i;
+  }
+  return start;
 };
 
 // Do not edit below this line
